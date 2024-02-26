271371d3bf5d9dd9b9c1264e58dea6fdae2e7281
torres-leo <leojts24@gmail.com>
2024-02-25 18:03:55 -0600

    styles: add the layer utilities from tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 import animations from 'tailwindcss-animated';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			xs: '500px',
+			...defaultTheme.screens,
+		},
+		extend: {
+			keyframes: {
+				gradient: {
+					'0%': { backgroundPosition: '0% 50%' },
+					'100%': { backgroundPosition: '100% 50%' },
+				},
+			},
+			animation: {
+				gradient: 'gradient 6s linear infinite',
+			},
+		},
 	},
 	plugins: [animations],
 };
