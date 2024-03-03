08fddb2623a320d338a07b84c1851315eab6d0cb
torres-leo <leojts24@gmail.com>
2024-03-03 02:06:18 -0600

    feat: finish bento

---
+++ src/data/BentoInfo.ts
@@ -0,0 +1,40 @@
+import type { BentoItem } from '@models/types';
+
+export const BentoInfo: BentoItem[] = [
+	{
+		id: 1,
+		title: 'ANDORRA: EL CÉNIT DE LOS PREMIOS ESLAND',
+		image: '/images/mountain.webp',
+		content: `<p>
+			Una nueva edición <strong>cargada de novedades</strong> y contenido, que empezará varias semanas antes de la ansiada
+			gala. <strong>Directos especiales, colaboraciones, conciertos y mucho más</strong> en lo que será una auténtica fiesta
+			dedicada a la creación de contenido.
+		</p>`,
+	},
+	{
+		id: 2,
+		title: 'LA ALFOMBRA BLANCA Y LA GALA',
+		subtitle: '16 de febrero de 2024',
+		image: '/images/grefg.webp',
+		content: `<p><strong>Uno de los días más esperados del año</strong> en la comunidad hispanohablante, <strong>con dos grandes hitos concentrados en un mismo directo</strong>. Una alfombra por la que pasarán los mejores streamers del año, luciendo sus mejores galas, momentos antes de saber si son los premiados.</p>
+
+			<p> Y una gala llena de emoción, <strong>con la entrega de premios, conciertos de artistas internacionales, y una gran cantidad de contenido</strong> pensado exclusivamente para la comunidad de streaming hispanohablante.</p>
+
+			<p>No te lo pierdas.</p>`,
+	},
+	{
+		id: 3,
+		title: 'LA GALA',
+		image: '/images/pistas.webp',
+		content: `<p>
+			Este año, el momento de la alfombra azul será sustituido y mejorado, gracias a los increibles paisajes andorranos, por una <strong>alfombra blanca en plena nieve</strong>, situada en las <strong>pistas de esquí de Andorra</strong>.
+		</p>`,
+	},
+	{
+		id: 4,
+		title: 'DISFRUTA DEL REDDIT DE LA COMUNIDAD',
+		image: '/images/laptop.webp',
+		content: `<p>
+			Este año, el reddit no formará parte de las votaciones de los premios, pero sí que <strong>formará parte de la comunidad</strong>. El subreddit oficial de los Premios ESLAND está abierto para ti, para que compartas tus clips, opiniones e <strong>interactúa con toda la comunidad</strong> del streaming hispanohablante.</p>`,
+	},
+];
---
+++ src/models/types/BentoItem.ts
@@ -0,0 +1,7 @@
+export type BentoItem = {
+	id: number;
+	title?: string;
+	image?: string;
+	subtitle?: string;
+	content?: string;
+};
