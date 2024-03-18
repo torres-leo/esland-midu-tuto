8846738f664a13b7a727980823ebb7eb43a2e8ab
torres-leo <leojts24@gmail.com>
2024-03-17 18:30:42 -0600

    feat: numbers in ESLAND

---
+++ src/components/NumerosItem.tsx
@@ -0,0 +1,15 @@
+import styles from '@/components/styles/NumerosItem.module.scss';
+
+interface Props {
+	title: string;
+	children?: preact.ComponentChildren;
+}
+
+export default function NumerosItem({ title, children }: Props) {
+	return (
+		<div class='flex flex-col justify-center items-center'>
+			<span class={`${styles.number} text-7xl font-bold tabular-nums tracking-tighter`}>{children}</span>
+			<span class='uppercase opacity-70'>{title}</span>
+		</div>
+	);
+}
